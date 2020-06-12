2cb96cd80574b7b7c1edf6d73eb1c799352ac20d
Shoro2 <info@ltdstats.com>
2020-06-12 10:14:38 +0200

    update

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
+// Legion TD 2 Ladder Parser:
+// Reads a json containing game events and writes the most recent overallElo of each player to an output json.
+// Download input files for each season: https://drive.google.com/drive/folders/1NIlguHLU3tP4_e5pTMQm75F-fMPIL6_s
+// Author: GvR Mr Mister
+// Github: https://github.com/Shoro2/ltd2_ladder_parser
+// Config:
+const SEASON        =   2
+const PATH_INPUT    =   "input/season" + SEASON + ".json";
+const PATH_OUTPUT   =   "output/ladders" + SEASON + ".json";
+//
 const fs = require('fs');
 var players = [];
-// INPUT
-var stream = fs.createReadStream("input/season2.json", {flags: 'r', encoding: 'utf-8'});
+try{
+    var stream = fs.createReadStream(PATH_INPUT, {flags: 'r', encoding: 'utf-8'});
+}
+catch(err){
+    console.log("Failed to open " + PATH_INPUT + ". Error: " + err);
+}
+
 var buf = '';
 
 stream.on('data', function(d) {
@@ -10,17 +25,23 @@ stream.on('data', function(d) {
 });
 
 stream.on('close', function(){
+    // sort by elo and write to output file
     players.sort((a, b) => (a.elo > b.elo) ? -1 : 1)
-    console.log("total players: "+players.length);
+    console.log("Total players: " + players.length);
     console.log(players[0]);
-    console.log(players[players.length -1]);
-    var rank=1;
-    players.forEach((ele) =>{
-        ele.rank = rank;
-        rank++;
-        // OUTPUT
-        fs.appendFileSync("output/ladders2.json", JSON.stringify(ele)+"\r\n");
-    });
+    console.log(players[players.length - 1]);
+    var rank = 1;
+    try{
+        players.forEach(ele =>{
+            ele.rank = rank;
+            rank++;
+            // OUTPUT
+            fs.appendFileSync(PATH_OUTPUT, JSON.stringify(ele) + "\r\n");
+        });
+    }
+    catch(err){
+        console.log("Failed to write to " + PATH_OUTPUT + ". Error: " + err);
+    }
 });
 
 function pump() {
@@ -30,27 +51,29 @@ function pump() {
             buf = buf.slice(1); // discard it
             continue; // so that the next iteration will start with data
         }
-        processLine(buf.slice(0,pos)); // hand off the line
-        buf = buf.slice(pos+1); // and slice the processed data off the buffer
+        processLine(buf.slice(0, pos)); // hand off the line
+        buf = buf.slice(pos + 1); // and slice the processed data off the buffer
     }
 }
 
 function processLine(line) { // here's where we do something with a line
-    if (line[line.length-1] == '\r') line=line.substr(0,line.length-1); // discard CR (0x0D)
+    if (line[line.length - 1] == '\r') line = line.substr(0, line.length - 1); // discard CR (0x0D)
     if (line.length > 0) { // ignore empty lines
         var obj = JSON.parse(line); // parse the JSON
-        obj.gameDetails.forEach(function(ele){
+        obj.gameDetails.forEach(ele =>{
             // some players overallElo is 0...
-            if(ele.overallElo>0){
+            if(ele.overallElo > 0){
                 // create new object
                 if(!players.find(x => x.name === ele.playername)){
+                    // ts = last game played this season
                     players.push({name: ele.playername, elo: ele.overallElo, ts: obj.ts});
                 }
                 // update existing object
                 else{
-                    if(obj.ts > players[players.findIndex(x => x.name === ele.playername)].ts){
-                        players[players.findIndex(x => x.name === ele.playername)].elo = ele.overallElo;
-                        players[players.findIndex(x => x.name === ele.playername)].ts = obj.ts;
+                    var player = players[players.findIndex(x => x.name === ele.playername)];
+                    if(obj.ts > player.ts){
+                        player.elo = ele.overallElo;
+                        player.ts = obj.ts;
                     }
                 }
             }
